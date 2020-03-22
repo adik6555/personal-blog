ecffa72db74b0417cfc526df3a45863c4b3ffff6
adik6555 <adik6555@gmail.com>
2020-03-22 13:05:51 +0100

    removed testing text and div

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,20 +6,17 @@ import "./index.css";
 
 export default function IndexPage({ data }) {
   return (
-    <div>
-      hey hey hey
-      <Layout>
-        {data.allMarkdownRemark.edges.map(({ node }) => (
-          <PostPreview
-            title={node.frontmatter.title}
-            date={node.frontmatter.date}
-            description={node.frontmatter.description}
-            link={node.fields.slug}
-            image={node.frontmatter.thumbnail.childImageSharp.fluid}
-          />
-        ))}
-      </Layout>
-    </div>
+    <Layout>
+      {data.allMarkdownRemark.edges.map(({ node }) => (
+        <PostPreview
+          title={node.frontmatter.title}
+          date={node.frontmatter.date}
+          description={node.frontmatter.description}
+          link={node.fields.slug}
+          image={node.frontmatter.thumbnail.childImageSharp.fluid}
+        />
+      ))}
+    </Layout>
   );
 }
 
